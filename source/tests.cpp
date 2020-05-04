0e48759df5a198ee0ae22ad141403b14c41e3386
Jakob Ruckel <jakob@ruckel.de>
2020-05-04 18:45:31 +0200

    add 1.8: gcd w/ tests

--- source/tests.cpp
+++ source/tests.cpp
@@ -1,6 +1,22 @@
 #define CATCH_CONFIG_RUNNER
 #include <catch.hpp>
 
+/** 1.8:  Euclidean Algorithm */
+int gcd(int a, int b) {
+    int r = a % b;
+    if (r == 0)
+        return b;
+    return gcd(b, r);
+}
+
+TEST_CASE("describe_gcd", "[gcd]") {
+    REQUIRE(gcd(2,4) == 2);
+    REQUIRE(gcd(3,7) == 1);
+    REQUIRE(gcd(7,3) == 1);
+    REQUIRE(gcd(9,6) == 3);
+    REQUIRE(gcd(41*42,42*43) == 42);
+}
+
 int main(int argc, char* argv[])
 {
   return Catch::Session().run(argc, argv);
